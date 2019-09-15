9957cb4902a559a3b81b6c2b24ec086d27291361
Zane Campbell <zanegitinit@gmail.com>
2019-09-14 18:33:46 -0700

    added NavBar

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,17 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import NavBar from './components/NavBar';
 import './App.css';
 
 function App() {
-  return <div className='App'>Hello</div>;
+  return (
+    <Router>
+      <NavBar />
+      <Fragment>
+        <Switch>{/* <Route exact path='/' component={Landing} /> */}</Switch>
+      </Fragment>
+    </Router>
+  );
 }
 
 export default App;
