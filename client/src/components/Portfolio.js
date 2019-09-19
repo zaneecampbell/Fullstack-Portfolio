6569f7ec2abd9b1b94d35bf6e0e1e36aa1534ecf
Zane Campbell <zanegitinit@gmail.com>
2019-09-18 20:08:27 -0700

    created Voting app portfolio piece

--- client/src/components/Portfolio.js
+++ client/src/components/Portfolio.js
@@ -27,7 +27,7 @@ const Portfolio = () => {
           src='https://i.imgur.com/6nMLf.jpg'
           alt='duck'
         />
-        <Grid style={{ background: '#e6e6e6', maxWidth: '700px' }} md={8} item>
+        <Grid style={{ background: '#e6e6e6', maxWidth: '700px' }} md={12} item>
           <Typography variant='h2'>Digital Artist Portfolio Website</Typography>
           <Typography style={{ fontSize: '1.4rem' }}>
             Lorem ipsum, dolor sit amet consectetur adipisicing elit. Numquam
@@ -40,16 +40,21 @@ const Portfolio = () => {
       <Grid style={{ marginTop: '100px' }} justify='center' container>
         <img
           style={{ width: '100%', maxWidth: '700px' }}
-          src='https://i.imgur.com/6nMLf.jpg'
+          src='portfolio_images/Voting_App.JPG'
           alt='duck'
         />
-        <Grid style={{ background: '#e6e6e6', maxWidth: '700px' }} md={8} item>
-          <Typography variant='h2'>Voting App</Typography>
-          <Typography style={{ fontSize: '1.4rem' }}>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Numquam
-            quasi voluptas accusamus officiis sunt distinctio fuga maiores
-            magnam, consequuntur praesentium, saepe iste minus! Repellat animi
-            officiis suscipit unde sapiente rerum soluta?
+        <Grid style={{ background: '#e6e6e6', maxWidth: '700px' }} md={12} item>
+          <Typography style={{ textAlign: 'center' }} variant='h2'>
+            Voting App
+          </Typography>
+          <Typography
+            style={{ fontSize: '1.4rem', marginTop: '30px', margin: '15px' }}
+          >
+            A fullstack voting appliation built using the MERN stack. The Create
+            pages form adds more fields as the last field available is used. I
+            prevent multiple votes through setting id values in localstorage and
+            checking them. The database Results page is updated in real-time for
+            user convenience. Styled with Material-UI.
           </Typography>
         </Grid>
       </Grid>
