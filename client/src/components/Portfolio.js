7cf96753a1be799be68ba1d922f9e378f0673c19
Zane Campbell <zanegitinit@gmail.com>
2019-09-20 18:24:09 -0700

    added fade effect for portfolio pieces

--- client/src/components/Portfolio.js
+++ client/src/components/Portfolio.js
@@ -12,121 +12,136 @@ const Portfolio = () => {
         marginBottom: '100px'
       }}
     >
-      <Grid justify='center' container>
-        <a
-          href='https://fullstack-inventory-app.herokuapp.com/'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          <img
+      <section data-aos='fade-left'>
+        <Grid justify='center' container>
+          <a
+            href='https://fullstack-inventory-app.herokuapp.com/'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            <img
+              style={{
+                width: '100%',
+                maxWidth: '700px',
+                verticalAlign: 'text-bottom'
+              }}
+              src='portfolio_images/Inventory_app.JPG'
+              alt='Inventory App'
+            />
+          </a>
+          <Grid
             style={{
-              width: '100%',
-              maxWidth: '700px',
-              verticalAlign: 'text-bottom'
+              background: '#e6e6e6',
+              maxWidth: '700px'
             }}
-            src='portfolio_images/Inventory_app.JPG'
-            alt='Inventory App'
-          />
-        </a>
-        <Grid
-          style={{
-            background: '#e6e6e6',
-            maxWidth: '700px'
-          }}
-          md={12}
-          item
-        >
-          <Typography
-            style={{ textAlign: 'center', marginTop: '30px' }}
-            variant='h2'
+            md={12}
+            item
           >
-            Inventory App
-          </Typography>
-          <Typography
-            style={{ fontSize: '1.4rem', marginTop: '30px', margin: '15px' }}
-          >
-            My biggest project to date utilizing as many techniques and
-            technologies that I could, built using the MERN stack. The FrontEnd
-            is built using React, React-Redux w/ thunk middleware, React-Router
-            with authentication checking, and material-ui for design. Backend
-            server is built on top of Express, passwords hashed in database with
-            bcryptjs and tracked with jsonwebtokens, validation utilizing
-            express validator, and mongodb/mongoose for database management.
-            Hosted on heroku.
-          </Typography>
+            <Typography
+              style={{ textAlign: 'center', marginTop: '30px' }}
+              variant='h2'
+            >
+              Inventory App
+            </Typography>
+            <Typography
+              style={{ fontSize: '1.4rem', marginTop: '30px', margin: '15px' }}
+            >
+              My biggest project to date utilizing as many techniques and
+              technologies that I could, built using the MERN stack. The
+              FrontEnd is built using React, React-Redux w/ thunk middleware,
+              React-Router with authentication checking, and material-ui for
+              design. Backend server is built on top of Express, passwords
+              hashed in database with bcryptjs and tracked with jsonwebtokens,
+              validation utilizing express validator, and mongodb/mongoose for
+              database management. Hosted on heroku.
+            </Typography>
+          </Grid>
         </Grid>
-      </Grid>
-      <Grid style={{ marginTop: '100px' }} justify='center' container>
-        <a
-          style={{ background: 'white' }}
-          href='https://choconov-portfolio.herokuapp.com/'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          <img
-            style={{
-              width: '100%',
-              maxWidth: '700px',
-              verticalAlign: 'text-bottom'
-            }}
-            src='portfolio_images/ChocoNov_Website.png'
-            alt='duck'
-          ></img>
-        </a>
-        <Grid style={{ background: '#e6e6e6', maxWidth: '700px' }} md={12} item>
-          <Typography
-            style={{ textAlign: 'center', marginTop: '30px' }}
-            variant='h2'
+      </section>
+      <section data-aos='fade-right'>
+        <Grid style={{ marginTop: '100px' }} justify='center' container>
+          <a
+            style={{ background: 'white' }}
+            href='https://choconov-portfolio.herokuapp.com/'
+            target='_blank'
+            rel='noopener noreferrer'
           >
-            Digital Artist Portfolio
-          </Typography>
-          <Typography
-            style={{ fontSize: '1.4rem', marginTop: '30px', margin: '15px' }}
+            <img
+              style={{
+                width: '100%',
+                maxWidth: '700px',
+                verticalAlign: 'text-bottom'
+              }}
+              src='portfolio_images/ChocoNov_Website.png'
+              alt='duck'
+            ></img>
+          </a>
+          <Grid
+            style={{ background: '#e6e6e6', maxWidth: '700px' }}
+            md={12}
+            item
           >
-            My first freelance project a portfolio website for a twitch
-            streamer/digital artists. Built using the MERN stack. Uses
-            ReactStrap as an intro to Bootstrap components for styling and
-            React-Router for routing.. Email form runs server side sent through
-            an online api to request commissions of the artists. Hosted on
-            heroku.
-          </Typography>
+            <Typography
+              style={{ textAlign: 'center', marginTop: '30px' }}
+              variant='h2'
+            >
+              Digital Artist Portfolio
+            </Typography>
+            <Typography
+              style={{ fontSize: '1.4rem', marginTop: '30px', margin: '15px' }}
+            >
+              My first freelance project a portfolio website for a twitch
+              streamer/digital artists. Built using the MERN stack. Uses
+              ReactStrap as an intro to Bootstrap components for styling and
+              React-Router for routing.. Email form runs server side sent
+              through an online api to request commissions of the artists.
+              Hosted on heroku.
+            </Typography>
+          </Grid>
         </Grid>
-      </Grid>
-      <Grid style={{ marginTop: '100px' }} justify='center' container>
-        <a
-          style={{ background: 'rgb(230, 230, 230)' }}
-          href='https://chill-polls.herokuapp.com/'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          <img
-            style={{
-              width: '100%',
-              maxWidth: '700px',
-              verticalAlign: 'text-bottom'
-            }}
-            src='portfolio_images/Voting_App.JPG'
-            alt='Voting App'
-          />
-        </a>
-        <Grid style={{ background: '#e6e6e6', maxWidth: '700px' }} md={12} item>
-          <Typography
-            style={{ textAlign: 'center', marginTop: '30px' }}
-            variant='h2'
+      </section>
+      <section data-aos='fade-left'>
+        <Grid style={{ marginTop: '100px' }} justify='center' container>
+          <a
+            style={{ background: 'rgb(230, 230, 230)' }}
+            href='https://chill-polls.herokuapp.com/'
+            target='_blank'
+            rel='noopener noreferrer'
           >
-            Voting App
-          </Typography>
-          <Typography
-            style={{ fontSize: '1.4rem', marginTop: '30px', margin: '15px' }}
+            <img
+              style={{
+                width: '100%',
+                maxWidth: '700px',
+                verticalAlign: 'text-bottom'
+              }}
+              src='portfolio_images/Voting_App.JPG'
+              alt='Voting App'
+            />
+          </a>
+          <Grid
+            style={{ background: '#e6e6e6', maxWidth: '700px' }}
+            md={12}
+            item
           >
-            A fullstack voting appliation built using the MERN stack. The Create
-            pages form adds more fields as the last field available is used. I
-            prevent multiple votes through setting id values in localstorage and
-            checking them. The database Results page is updated in real-time for
-            user convenience. Styled with Material-UI. Hosted on heroku.
-          </Typography>
+            <Typography
+              style={{ textAlign: 'center', marginTop: '30px' }}
+              variant='h2'
+            >
+              Voting App
+            </Typography>
+            <Typography
+              style={{ fontSize: '1.4rem', marginTop: '30px', margin: '15px' }}
+            >
+              A fullstack voting appliation built using the MERN stack. The
+              Create pages form adds more fields as the last field available is
+              used. I prevent multiple votes through setting id values in
+              localstorage and checking them. The database Results page is
+              updated in real-time for user convenience. Styled with
+              Material-UI. Hosted on heroku.
+            </Typography>
+          </Grid>
         </Grid>
-      </Grid>
+      </section>
     </div>
   );
 };
