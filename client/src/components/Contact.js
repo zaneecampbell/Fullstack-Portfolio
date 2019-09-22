c4968e13f92a553284293592da331a61284f1fa9
Zane Campbell <zanegitinit@gmail.com>
2019-09-21 21:39:32 -0700

    remade form

--- client/src/components/Contact.js
+++ client/src/components/Contact.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import Input from '@material-ui/core/Input';
+import Grid from '@material-ui/core/Grid';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import { Typography } from '@material-ui/core';
@@ -43,67 +43,73 @@ const Contact = () => {
       </Typography>
       <div
         style={{
-          maxWidth: '960px',
+          maxWidth: '600px',
           margin: 'auto',
           paddingBottom: '25px',
           paddingTop: '25px'
         }}
       >
         <form onSubmit={e => onSubmit(e)}>
-          <Input
-            style={{
-              fontSize: '25px',
-              margin: '5px',
-              width: '55%',
-              minWidth: '300px'
-            }}
-            type='email'
-            value={email}
-            name='email'
-            placeholder='Email here'
-            onChange={e => onChange(e)}
-            autoComplete='off'
-          />
-          <br />
-          <Input
-            style={{
-              fontSize: '25px',
-              margin: '5px',
-              width: '55%',
-              minWidth: '300px'
-            }}
-            type='name'
-            value={name}
-            name='name'
-            placeholder='Company/Name'
-            onChange={e => onChange(e)}
-            autoComplete='off'
-            required
-          />
-          <br />
-          <TextField
-            style={{
-              fontSize: '25px',
-              margin: '5px',
-              width: '55%',
-              minWidth: '300px'
-            }}
-            variant='outlined'
-            type='text'
-            multiline
-            rows='4'
-            value={text}
-            name='text'
-            placeholder="Let me know what's up"
-            onChange={e => onChange(e)}
-            autoComplete='off'
-            required
-          />
-          <div style={{ margin: 'auto', width: '55%', textAlign: 'right' }}>
+          <Grid container justify='center' spacing={0}>
+            <Grid xs={10} lg={6} item>
+              <TextField
+                style={{
+                  fontSize: '25px',
+                  margin: '5px',
+                  width: '100%'
+                }}
+                variant='outlined'
+                type='email'
+                value={email}
+                name='email'
+                placeholder='Email here'
+                onChange={e => onChange(e)}
+                autoComplete='off'
+              />
+            </Grid>
+            <Grid xs={10} lg={6} item>
+              <TextField
+                style={{
+                  fontSize: '25px',
+                  margin: '5px',
+                  width: '100%'
+                }}
+                variant='outlined'
+                type='name'
+                value={name}
+                name='name'
+                placeholder='Company/Name'
+                onChange={e => onChange(e)}
+                autoComplete='off'
+                required
+              />
+            </Grid>
+            <Grid xs={10} lg={12} item>
+              <TextField
+                style={{
+                  fontSize: '25px',
+                  margin: '5px',
+                  width: '100%',
+                  minWidth: '300px'
+                }}
+                variant='outlined'
+                type='text'
+                multiline
+                rows='4'
+                value={text}
+                name='text'
+                placeholder="Let me know what's up"
+                onChange={e => onChange(e)}
+                autoComplete='off'
+                required
+              />
+            </Grid>
+          </Grid>
+          <Grid xs={11} md={11} lg={12} style={{ textAlign: 'right' }} item>
             <Button
               style={{
                 marginTop: '25px',
-                fontSize: '15px',
+                fontSize: '20px',
                 backgroundColor: '#495057',
                 padding: '5px',
                 color: 'white'
@@ -112,7 +118,7 @@ const Contact = () => {
             >
               Submit
             </Button>
-          </div>
+          </Grid>
         </form>
       </div>
     </div>
