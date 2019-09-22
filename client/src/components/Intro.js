f59e7d557a78fc0ddf9fe6e223c58a5061d91c5b
Zane Campbell <zanegitinit@gmail.com>
2019-09-21 22:02:12 -0700

    formatting

--- client/src/components/Intro.js
+++ client/src/components/Intro.js
@@ -12,23 +12,29 @@ const Intro = () => {
         position: 'absolute',
         left: 0,
         right: 0,
-        top: '25%',
+        top: '20%',
         margin: 'auto',
         maxWidth: '500px'
       }}
     >
-      <img style={{ maxWidth: '200px', textAlign: 'center' }} src='/me.png' alt='Profile of Zane.' />
-      <Typography
-        style={{
-          color: 'white',
-          textAlign: 'center',
-          fontSize: '2rem',
-          fontWeight: '300'
-        }}
-      >
-        Hi, My name is Zane. I'm a Fullstack Web Developer with a focus on the
-        MERN stack!
-      </Typography>
+      <img
+        style={{ maxWidth: '200px', textAlign: 'center' }}
+        src='/me.png'
+        alt='Profile of Zane.'
+      />
+      <div style={{ marginRight: '15px', marginLeft: '15px' }}>
+        <Typography
+          style={{
+            color: 'white',
+            textAlign: 'center',
+            fontSize: '2rem',
+            fontWeight: '300'
+          }}
+        >
+          Hi, My name is Zane. I'm a Fullstack Web Developer with a focus on the
+          MERN stack!
+        </Typography>
+      </div>
     </div>
   );
 };
