73f18c7b936087417d40d8ecfe4bf2bd11aa5aaf
Zane Campbell <zanegitinit@gmail.com>
2019-09-29 18:17:21 -0700

    initial attempt at aws launch

---
+++ client/build/precache-manifest.c09cf6ee2bfb8ae88306c7d3c32a7bb8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34b84f9a837e669e896141daab7eca25",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6be1d2ce1083cb2529fb",
+    "url": "/static/css/main.9ee2e248.chunk.css"
+  },
+  {
+    "revision": "7d95235c7adea7f8147f",
+    "url": "/static/js/2.7cf605dc.chunk.js"
+  },
+  {
+    "revision": "6be1d2ce1083cb2529fb",
+    "url": "/static/js/main.d1de6dde.chunk.js"
+  },
+  {
+    "revision": "b3938888db487c1703c6",
+    "url": "/static/js/runtime~main.a7ca617d.js"
+  }
+]);
