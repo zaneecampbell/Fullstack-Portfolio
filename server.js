370a1000d5148974e9ef5ef63bc276f5ce1da1b1
Zane Campbell <zanegitinit@gmail.com>
2019-09-30 19:37:01 -0700

    added comments for productioon purposes

--- server.js
+++ server.js
@@ -9,16 +9,24 @@ app.use(express.json({ extended: false }));
 // Defined Route/s
 app.use('/api/email', require('./routes/api/email'));
 
-// Serve static assets in production
-if (process.env.NODE_ENV === 'production') {
-  // Set static folder
-  app.use(express.static('client/build'));
+// Set static folder
+app.use(express.static('client/build'));
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
-}
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+});
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+
+// Heroku ver
+// Serve static assets in production
+// if (process.env.NODE_ENV === 'production') {
+//   // Set static folder
+//   app.use(express.static('client/build'));
+
+//   app.get('*', (req, res) => {
+//     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+//   });
+// }
